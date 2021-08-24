21d6e12231743ccbf48e4da6245680310e2e47f1
muskanmishra4 <73119508+muskanmishra4@users.noreply.github.com>
2021-08-24 09:49:29 +0530

    Create demo2.tf

---
+++ demo2.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+    
+  }
+  
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+
+
+
+
+
+
